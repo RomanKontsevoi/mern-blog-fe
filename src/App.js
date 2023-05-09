0b9a49954dd6c86a3eee208aea1c2bd7300a04ad
RomanKontsevoi <3844320@gmail.com>
2023-05-09 12:23:40 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import Container from "@mui/material/Container";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Container from '@mui/material/Container'
 
-import { Header } from "./components";
-import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import { Header } from './components'
+import { AddPost, FullPost, Home, Login, Registration } from './pages'
+
+const layout = (Component) => (
+  <>
+    <Header />
+    <Container maxWidth="lg">
+      <Component />
+    </Container>
+  </>
+)
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: layout(Home),
+  },
+  {
+    path: '/posts/:id',
+    element: layout(FullPost),
+  },
+  {
+    path: '/add-post',
+    element: layout(AddPost),
+  },
+  {
+    path: '/login',
+    element: layout(Login),
+  },
+  {
+    path: '/register',
+    element: layout(Registration),
+  },
+])
 
 function App() {
   return (
-    <>
-      <Header />
-      <Container maxWidth="lg">
-        <Home />
-        {/*<FullPost />*/}
-        {/*<AddPost />*/}
-        {/*<Login />*/}
-        {/*<Registration />*/}
-      </Container>
-    </>
-  );
+    <RouterProvider router={router} />
+  )
 }
 
-export default App;
+export default App
