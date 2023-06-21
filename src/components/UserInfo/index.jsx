af071c5e2ee6b3d692f20848dfe5c4bdaff86394
RomanKontsevoi <3844320@gmail.com>
2023-06-21 20:43:11 +0300

    fixed bug with user icon

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -7,7 +7,7 @@ export const UserInfo = ({ avatarUrl, fullName, additionalText }) => {
     <div className={styles.root}>
       <img
         className={styles.avatar}
-        src={`${baseURL}${avatarUrl}` || '/user_icon.png'}
+        src={avatarUrl ? `${baseURL}${avatarUrl}` : `${baseURL}/user_icon.png`}
         alt={fullName}
       />
       <div className={styles.userDetails}>
